a6007477b4262479e3a963167f9a6f138386b3f3
pawelNu <pnurzynski17@gmail.com>
2024-01-14 11:54:01 +0100

    formik simple with yup validation

---
+++ forms/src/components/formik/FormSimpleFormikYupValidation.tsx
@@ -0,0 +1,31 @@
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
+interface FormValues {
+  userName: string;
+}
+
+function FormSimpleFormikYupValidation() {
+  return (
+    <Formik
+      initialValues={{ userName: '' }}
+      validationSchema={Yup.object({
+        userName: Yup.string()
+          .max(10, 'At most 10 characters')
+          .min(5, 'At least 5 characters')
+          .required('Name is required'),
+      })}
+      onSubmit={(values: FormValues) => alert(JSON.stringify(values))}
+    >
+      {() => (
+        <Form>
+          <Field type="text" name="userName" />
+          <ErrorMessage name="userName" component="div" />
+          <button type="submit">Submit</button>
+        </Form>
+      )}
+    </Formik>
+  );
+}
+
+export default FormSimpleFormikYupValidation;
