b17300033058d70414dbaec570f227416b014581
pawelNu <pnurzynski17@gmail.com>
2024-01-14 11:27:36 +0100

    reaname parent directory to forms

---
+++ forms/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import SimpleForm from "./components/SimpleForm";
+
+function App() {
+    return (
+        <>
+            <SimpleForm></SimpleForm>
+        </>
+    );
+}
+
+export default App;
